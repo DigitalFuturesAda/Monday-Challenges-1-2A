35ec881179ed5fc25c3612bf8d8e88963544cc9f
Lyons <58537339+DigitalFuturesAda@users.noreply.github.com>
2021-01-11 20:11:16 +0000

    added documentation because I am an advanced programmer

--- E1.cpp
+++ E1.cpp
@@ -3,15 +3,20 @@
 #include <iostream>
 
 int main() {
+    // declare variables
     std::string author;
     std::string quote;
 
+    // pipe str into out
     std::cout << "What is the quote? ";
     getline(std::cin, quote);
+    // pipe in into quote
 
     std::cout << "Who said it? ";
     getline(std::cin, author);
+    // ditto
 
+    // uses escape characters to posiiton " inside of ""
     std::cout << author << " says: \"" << quote << "\"" << std::endl;
 
     return 0;
