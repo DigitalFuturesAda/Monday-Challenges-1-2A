1081a8dabd4d3087d7769af75393d5076c0a08d5
Lyons <58537339+DigitalFuturesAda@users.noreply.github.com>
2021-01-11 20:08:52 +0000

    Added docuemntation

--- E2.cpp
+++ E2.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <algorithm>
 
+// converts string to lowercase by iterating through each char and calling tolower on it
 void convertToLowerCase(std::string &str) {
     std::transform(str.begin(), str.end(), str.begin(),
                    [](unsigned char c){ return std::tolower(c); });
 }
 
+// declares an enum 
 enum Temperature {
     Centigrade, Fahrenheit
 };
@@ -21,6 +23,7 @@ double getTemp() {
 
     try {
         temp = std::stod(tempInput);
+        // input validation, ensures the input is a double.
     } catch(const std::exception&) {
         std::cout << "Invalid input, please enter an integer or float." << std::endl;
         return getTemp();
@@ -36,8 +39,10 @@ Temperature getConversion() {
         << "Press ‘F’ to convert from Centigrade to Fahrenheit." << std::endl << std::endl;
     std::cout << "Your choice: ";
 
+    // getline pipes the input into the variable
     getline(std::cin, conversion);
     convertToLowerCase(conversion);
+    // converts variable to lowercase
 
     if (conversion.size() == 1){
         if (conversion == "c"){
@@ -62,7 +67,8 @@ int main() {
     } else {
         convertedOutput = (inputTemp * 9 / 5) + 32;
     }
-
+    
+    // ternary operator, pretty much a nested if statement
     std::cout << inputTemp << " degrees " << (conversion == Centigrade ? "Fahrenheit" : "Centigrade") <<  " is "
         << convertedOutput << " degrees " << (conversion == Centigrade ? "Centigrade" : "Fahrenheit");
 }
